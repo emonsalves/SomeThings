39c6b8a18e38cdeb7be7940271baa76ac62f29f4
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-25 23:44:40 -0300

    Fix: btns

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,13 +6,17 @@ function Counter() {
   const [counter, setCounter] = useState(0)
   //   set actions btns
   const handleAdd = () => setCounter(counter + 1)
+  const handleAdd10 = () => setCounter(counter + 10)
   const HandleSubstract = () => setCounter(counter - 1)
+  const HandleSubstract10 = () => setCounter(counter - 10)
   const handleReset = () => setCounter(0)
   // set objet btns
   const buttons = [
     { id: 1, name: " +1 ", action: handleAdd },
     { id: 2, name: " -1 ", action: HandleSubstract },
     { id: 3, name: " Reset ", action: handleReset },
+    { id: 4, name: " +10 ", action: handleAdd10 },
+    { id: 5, name: " -10 ", action: HandleSubstract10 },
   ]
   return (
     <>
