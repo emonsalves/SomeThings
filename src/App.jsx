6a273450e9c35f2c27b14106d72ce25740f0b863
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-28 16:43:49 -0300

    Added: Api fetch try 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react"
 import PageLayout from "./layout/PageLayout"
-import MyContext from "./Context/MyContext";
+import MyContext from "./Context/MyContext"
 
 function App() {
-  const { test } = useContext(MyContext);
+  const { apiUrl, data, setData } = useContext(MyContext)
 
   return (
     <div className="App">
-      <PageLayout/>
+      {/* <PageLayout /> */}
+      {data.map((dato, index) => (
+        <div key={`${dato.name}-${index}`} className="flex flex-col justify-center items-center">
+          <h1>{dato.name}</h1>
+          <img src={dato.img} alt="" />
+          {dato.like && <h2>Like</h2>}
+        </div>
+      ))}
     </div>
   )
 }
--- src/Context/MyProvider.jsx
+++ src/Context/MyProvider.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
 import MyContext from "./MyContext"
 
 const MyContextProvider = ({ children }) => {
-  const [test, setTest] = useState("Test Context")
+  const [data, setData] = useState([])
+  const apiUrl = "https://digimon-api.vercel.app/api/digimon"
+
+  const getData = async (apiUrl) => {
+    const data = await fetch(apiUrl).then((res) => res.json())
+    const data2 = await data.map((x) => ({ ...x, like: false }))
+    setData(data2)
+    console.log(data2)
+  }
+
+  useEffect(() => {
+    getData(apiUrl)
+  }, [])
 
   return (
-    <MyContext.Provider value={{ test, setTest }}>
+    <MyContext.Provider value={{ data, setData, apiUrl }}>
       {children}
     </MyContext.Provider>
   )
